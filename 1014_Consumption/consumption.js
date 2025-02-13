c8902627c451d0926810e2da70e371917af3e134
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-13 18:14:58 +0000

    fix(all js archives): removing soving code
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

--- 1014_Consumption/consumption.js
+++ 1014_Consumption/consumption.js
@@ -1,15 +1,4 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const [A, B] = input.split('\n').map(parseFloat);
 
-const averageConsuption = (X, Y) => {
-    const totalDistanceKM = X;
-    const totalCoastGasoline = Y;
-    const averageConsumptionCar = totalDistanceKM/totalCoastGasoline;
 
-    return averageConsumptionCar.toFixed(3);
-};
-
-const AVERAGE_CONSUMPTION = (X,Y) => {return (X / Y).toFixed(3) }
-
-console.log(`${averageConsuption(A, B)} km/l`);
-console.log(`${AVERAGE_CONSUMPTION(A, B)} km/l`);
