630e9e5dc939f83439c84da2e270267216573bb7
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-13 18:06:39 +0000

    feat(1014): add exercise 1014
    Usei um arrow function para tirar a media do consumo do carro a partir da divisão das duas entradas
    
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

--- 1014_Consumption/consumption.js
+++ 1014_Consumption/consumption.js
@@ -1,2 +1,12 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const [A, B, C] = input.split('\n').map(parseFloat);
+const [A, B] = input.split('\n').map(parseFloat);
+
+const averageConsuption = (X, Y) => {
+    const totalDistanceKM = X;
+    const totalCoastGasoline = Y;
+    const averageConsumptionCar = totalDistanceKM/totalCoastGasoline;
+
+    return averageConsumptionCar.toFixed(3);
+};
+
+console.log(`${averageConsuption(A, B)} km/l`);
