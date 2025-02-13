80c7ad467e76c3d524874fbd1216a8eefb85adf5
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-13 18:10:08 +0000

    fix(1014): add arrow function alternative with main funciont
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

--- 1014_Consumption/consumption.js
+++ 1014_Consumption/consumption.js
@@ -9,4 +9,7 @@ const averageConsuption = (X, Y) => {
     return averageConsumptionCar.toFixed(3);
 };
 
+const AVERAGE_CONSUMPTION = (X,Y) => {return (X / Y).toFixed(3) }
+
 console.log(`${averageConsuption(A, B)} km/l`);
+console.log(`${AVERAGE_CONSUMPTION(A, B)} km/l`);
